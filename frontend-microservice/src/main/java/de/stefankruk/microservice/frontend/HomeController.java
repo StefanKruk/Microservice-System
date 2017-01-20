cc5037981a8e16279333931c45f35961d371937a
StefanKruk <Stefan.Kruk@gmx.net>
2017-01-20 16:04:23 +0100

    Added cloud configuration

--- frontend-microservice/src/main/java/de/stefankruk/microservice/frontend/HomeController.java
+++ frontend-microservice/src/main/java/de/stefankruk/microservice/frontend/HomeController.java
@@ -1,21 +1,22 @@
 package de.stefankruk.microservice.frontend;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RefreshScope
 @Controller
 public class HomeController {
 
     @Value("${my.name:World}")
     private String name;
 
+    @Value("${index}")
+    private String index;
+
     @RequestMapping("/")
     public String hello(Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return index;
     }
 }
