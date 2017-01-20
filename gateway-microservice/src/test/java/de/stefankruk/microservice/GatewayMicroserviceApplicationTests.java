b361d2958dd1bebb09e1740ef56af9e5fcfc46f0
StefanKruk <Stefan.Kruk@gmx.net>
2017-01-20 17:03:33 +0100

    Added Zuul as Gateway

---
+++ gateway-microservice/src/test/java/de/stefankruk/microservice/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.stefankruk.microservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
