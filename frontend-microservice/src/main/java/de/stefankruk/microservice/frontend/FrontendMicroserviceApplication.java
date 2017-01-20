fb747971370e0b84bf3311571254b957e4ff0a4c
Stefan <Stefan.Kruk@gmx.net>
2017-01-20 11:45:09 +0100

    Frontend aufgeräumt

--- frontend-microservice/src/main/java/de/stefankruk/microservice/frontend/FrontendMicroserviceApplication.java
+++ frontend-microservice/src/main/java/de/stefankruk/microservice/frontend/FrontendMicroserviceApplication.java
@@ -1,19 +1,26 @@
 package de.stefankruk.microservice.frontend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringCloudApplication
 public class FrontendMicroserviceApplication extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FrontendMicroserviceApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FrontendMicroserviceApplication.class);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(FrontendMicroserviceApplication.class, args);
+    }
+
+    @Bean
+    public HomeController homeController() {
+        return new HomeController();
     }
 }
